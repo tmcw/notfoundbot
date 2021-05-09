a2d812fc2442ee9cd7f7f093717ec366da6318e8
steveoh <sgourley@utah.gov>
2021-05-09 15:20:59 -0600

    feat: create exceptions list (#56)
    refs #52

--- cli.ts
+++ cli.ts
@@ -3,5 +3,6 @@ import { sniff } from "./src/check_links";
 // Usage: ts-node cli.ts <URL>
 
 (async function () {
-  console.log(await sniff(process.argv[2]));
+  const exceptions = process.env.EXCEPTIONS?.split(' ') ?? []
+  console.log(await sniff(process.argv[2], exceptions));
 })();
